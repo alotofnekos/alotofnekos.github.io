dc38ef501dafe113a3652af890dee55e3597f95e
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-05-31 18:12:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,26 +23,26 @@ document.getElementById("evsAtk").addEventListener("input", function() {
             }
 });
 document.getElementById("battleForm").addEventListener("submit", function(event) {
-      event.preventDefault();
+            event.preventDefault();
+            // Retrieve form values
+            var baseDef = parseInt(document.getElementById("baseDef").value);
+            var evsDef = parseInt(document.getElementById("evsDef").value);
+            var natureElement = document.querySelector('input[name="nature"]:checked');
+            var nature = parseFloat(natureElement.value);
+            var baseHP = parseInt(document.getElementById("baseHP").value);
+            var evsHP = parseInt(document.getElementById("evsHP").value);
+            var baseAtk = parseInt(document.getElementById("baseAtk").value);
+            var evsAtk = parseInt(document.getElementById("evsAtk").value);
+            var natureAtkElement = document.querySelector('input[name="natureAtk"]:checked');
+            var natureAtk = parseFloat(natureAtkElement.value);
+            var stabElement = document.querySelector('input[name="stab"]:checked');
+            var stab = parseInt(stabElement.value);
+            var basePower = parseInt(document.getElementById("basePower").value);
+            var effectiveness = parseFloat(document.querySelector('input[name="effectiveness"]:checked').value);
+            var desiredPercentage = parseFloat(document.getElementById("desiredPercentage").value);
+            // Perform calculations or further processing here
+            // ...
+            // Display or output the results
+            // ...
+});
 
-      // Retrieve form values
-      var baseDef = parseInt(document.getElementById("baseDef").value);
-      var evsDef = parseInt(document.getElementById("evsDef").value);
-      var natureElement = document.querySelector('input[name="nature"]:checked');
-      var nature = parseFloat(natureElement.value);
-      var baseHP = parseInt(document.getElementById("baseHP").value);
-      var evsHP = parseInt(document.getElementById("evsHP").value);
-      var baseAtk = parseInt(document.getElementById("baseAtk").value);
-      var evsAtk = parseInt(document.getElementById("evsAtk").value);
-      var natureAtk = parseFloat(document.getElementById("natureAtk").value);
-      var stab = parseInt(document.getElementById("stab").value);
-      var basePower = parseInt(document.getElementById("basePower").value);
-      var effectiveness = parseFloat(document.getElementById("effectiveness").value);
-      var desiredPercentage = parseFloat(document.getElementById("desiredPercentage").value);
-
-      // Perform calculations or further processing here
-      // ...
-
-      // Display or output the results
-      // ...
-    });
