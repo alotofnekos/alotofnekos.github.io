9fa1df406b171529bf6aef18144f0dba5c48c20f
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-05-31 17:17:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("battleForm").addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      // Retrieve form values
+      var baseDef = parseInt(document.getElementById("baseDef").value);
+      var evsDef = parseInt(document.getElementById("evsDef").value);
+      var nature = parseFloat(document.getElementById("nature").value);
+      var baseHP = parseInt(document.getElementById("baseHP").value);
+      var evsHP = parseInt(document.getElementById("evsHP").value);
+      var baseAtk = parseInt(document.getElementById("baseAtk").value);
+      var evsAtk = parseInt(document.getElementById("evsAtk").value);
+      var natureAtk = parseFloat(document.getElementById("natureAtk").value);
+      var stab = parseInt(document.getElementById("stab").value);
+      var basePower = parseInt(document.getElementById("basePower").value);
+      var effectiveness = parseFloat(document.getElementById("effectiveness").value);
+      var desiredPercentage = parseFloat(document.getElementById("desiredPercentage").value);
+
+      // Perform calculations or further processing here
+      // ...
+
+      // Display or output the results
+      // ...
+    });
